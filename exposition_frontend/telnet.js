7fd1170e1b396b0c2f8b9e954aeccb8da4516419
cmichi <cmichi@localhost>
2014-04-05 07:36:17 +0200

    telnet stuff fix

--- exposition_frontend/telnet.js
+++ exposition_frontend/telnet.js
@@ -1,6 +1,10 @@
 var net = require("net");
 
-var socket = net.createConnection(1337, "192.168.1.105");
-socket.on("data", function(data) {
-	console.log("" + data);
-});
+//try {
+	var socket = net.createConnection(1337, "192.168.1.105");
+	socket.on("data", function(data) {
+		console.log("" + data);
+	});
+//} catch (err) {
+	//console.log(err);
+//}
